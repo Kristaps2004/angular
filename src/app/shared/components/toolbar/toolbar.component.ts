ddb750196309b7b35b4528a9a4189e7e69b84264
kesmits0 <Kristaps.Esmits@tet.lv>
2023-06-22 11:37:56 +0300

    Added dark mode

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,4 +1,8 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, HostBinding, Output} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {ThemeService} from "../../services/theme.service";
+import {Observable, startWith} from "rxjs";
+import {MatSlideToggleChange} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-toolbar',
@@ -9,8 +13,22 @@ export class ToolbarComponent {
   @Output()
   public toggleSideNav: EventEmitter<void> = new EventEmitter<void>();
 
+  public isDarkTheme$:Observable<boolean>;
+
+  constructor(
+    public themeService: ThemeService,
+  ) {
+    this.isDarkTheme$ = this.themeService.isDarkTheme$
+      .pipe(
+        startWith(false)
+      );
+  }
   public onToggleSideNav(): void {
     this.toggleSideNav.emit();
+  }
+
+  onThemeChange(slideValue: MatSlideToggleChange) {
+    this.themeService.toggleDarkTheme(slideValue.checked)
   }
 }
 
