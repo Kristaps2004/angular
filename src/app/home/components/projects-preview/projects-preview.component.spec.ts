0a6e7e7bfa6a24b7217a431a8589e8fe5b1fcf99
kesmits0 <Kristaps.Esmits@tet.lv>
2023-06-22 14:40:45 +0300

    created new service for projects-preview component

---
+++ src/app/home/components/projects-preview/projects-preview.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectsPreviewComponent } from './projects-preview.component';
+
+describe('ProjectsPreviewComponent', () => {
+  let component: ProjectsPreviewComponent;
+  let fixture: ComponentFixture<ProjectsPreviewComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProjectsPreviewComponent]
+    });
+    fixture = TestBed.createComponent(ProjectsPreviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/services/repo-preview.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { RepoPreviewService } from './repo-preview.service';
+
+describe('RepoPreviewService', () => {
+  let service: RepoPreviewService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(RepoPreviewService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
