295471cdfabc0647f39dca3cd8889afa107de162
kesmits0 <Kristaps.Esmits@tet.lv>
2023-06-26 14:00:44 +0300

    Router auto top solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, Inject, ViewChild} from "@angular/core";
+import {NavigationEnd, Router} from "@angular/router";
+import {filter, tap} from "rxjs";
+import {DOCUMENT} from "@angular/common";
 
 
 @Component({
@@ -8,4 +11,23 @@ import {Component} from "@angular/core";
 })
 export class AppComponent {
 
+  private window :  Window;
+
+  @ViewChild('sidenav')
+  public sideNav!: ElementRef;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router,
+  ) {
+    this.window = this.document.defaultView as Window;
+    this.router.events
+      .pipe(
+        filter(
+          (event) => event instanceof NavigationEnd
+        ),
+        tap((event) => this.document.querySelector('.mat-sidenav-content')?.scrollTo(0, 0))
+      )
+      .subscribe();
+  }
 }
